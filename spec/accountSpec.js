eb954b462a490f18120c1fcab799554a32280201
Josué Estévez Fernández <jestfer6@gmail.com>
2018-03-26 14:55:22 +0100

    account initializes with an empty history, tested and passing

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -9,5 +9,9 @@ describe('Account', () => {
     it('it initializes with a balance of 0', () => {
       expect(account.balance).toEqual(0);
     });
+
+    it('it initializes with an empty history', () => {
+      expect(account.history).toEqual([]);
+    });
   });
 });
